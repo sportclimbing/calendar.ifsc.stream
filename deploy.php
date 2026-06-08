14ecf3ddfce50e2628d7e11fa8066ab1ed7044b2
Nico Oelgart <nicoswd@gmail.com>
2026-06-08 23:29:29 +0200

    Migrate to slim

--- deploy.php
+++ deploy.php
@@ -10,8 +10,8 @@ set('application', 'calendar-ifsc-stream');
 
 host('production')
     ->hostname('95.85.49.210')
-    ->user(getenv('DEPLOY_USER'))
-    ->set('deploy_path', '/home/{{user}}/web/cal.ifsc.stream/public_html')
+    ->user(getenv('DEPLOY_USER') ?: 'ifsc')
+    ->set('deploy_path', '/home/ifsc/web/cal.ifsc.stream/public_html')
     ->identityFile('~/.ssh/deploy_key');
 
 add('shared_dirs', ['var']);
